bf99198294663d7ebece2d25b2a4ec2c043526b5
Chena <chenarusky@gmail.com>
2022-05-14 11:27:42 -0500

    Agregue cosas

--- update_titulos.php
+++ update_titulos.php
@@ -60,7 +60,18 @@ require('funciones.php');
           </form>
         </div>
       </div>
+      <div>
+              <form method="post" action="">
+                <div class="form-group">
+                  <label for="copy_id" class="col-sm-">
+                    
+                  </label>
+                  </div>
+
+              </form>      
+        </div>
     </div>
+  
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
